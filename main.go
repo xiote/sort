7ef8a0d2216e76398608f32b5bafa8ffac53a273
xiote <xiote@nate.com>
2020-01-28 22:50:46 +0900

    Add main func

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	names := []string{}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		var name string = scanner.Text()
+
+		if len(name) == 0 {
+			sorted := Sort(names)
+			fmt.Printf("len=%d cap=%d %v\n", len(sorted), cap(sorted), sorted)
+			break
+		}
+
+		names = append(names, name)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
